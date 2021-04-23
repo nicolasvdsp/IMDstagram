6021cff92de1fea971ff306e0cf58afbf8dd5449
nicolasvdsp <nicolasvds@live.be>
2021-04-23 19:29:24 +0200

    adds a postcard in HTML

--- index.php
+++ index.php
@@ -5,10 +5,12 @@
     $conn = Db::getConnection();
     $statement = $conn->prepare("SELECT * FROM post");
     $statement->execute();
-    $user = $statement->fetch();
+    $posts = $statement->fetchAll();
 
-    echo $user["text"];
-    echo $user["image"];
+    foreach($posts as $post){
+        // echo $post['text'];
+        // echo "<img class='post__image' src='". $post['image'] ."' alt='post image'/>";
+    }
 
 ?><!DOCTYPE html>
 <html lang="en">
@@ -21,15 +23,37 @@
     <title>Dinkstagram</title>
 </head>
 <body>
+        <div class="header">
+            <img class="logo" src="./assets/logo_dinkstagram.svg" alt="Logo Dinkstagram"/>
+            <a href="#"><img class="search" src="./assets/icon_search.svg" alt="Search button"/></a>
+        </div>
+
+
+        <div class="post">
+            <div class="post__head">
+                <img class="post_userImage" src="assets/cesarAlien.jpg" alt="Profile Picture"/>
+                <p class="post_userName">Fons</p>
+            </div>
+            <img class="post__image" src="<?php echo $post['image']; ?>" alt="Post Image"/>
+            <div class="post__foot">
+                <div class="post__likes">
+                    <a href="#"><img src="assets/icon_likes.svg" alt="Number of likes"/></a>
+                    <span>5</span>
+                </div>
+                <div class="post__comments">
+                    <a href="#"><img src="assets/icon_comments.svg" alt="Number of comments"/></a>
+                    <span>5</span>
+                </div>
+            </div>
+        </div>
+
     
-    <div class="navigation">
         <nav class="navbar">
             <a class="navbar__btn" href="#">Home</a>
             <a class="navbar__btn" href="#">Add</a>
             <a class="navbar__btn" href="#">User</a>
             
         </nav>
-    </div>
     
 </body>
 </html>
