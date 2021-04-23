a7779d482026cf45f5c9b6ee22e91237397a39dc
nicolasvdsp <nicolasvds@live.be>
2021-04-23 12:00:16 +0200

    first styling

--- index.php
+++ index.php
@@ -3,11 +3,12 @@
     include_once(__DIR__ . "/classes/Db.php");
 
     $conn = Db::getConnection();
-    $statement = $conn->prepare("SELECT * FROM users");
+    $statement = $conn->prepare("SELECT * FROM post");
     $statement->execute();
     $user = $statement->fetch();
 
-    echo $user["firstname"];
+    echo $user["text"];
+    echo $user["image"];
 
 ?><!DOCTYPE html>
 <html lang="en">
@@ -15,11 +16,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="./style/style.css">
+    <link rel="stylesheet" type="text/css" href="style/style.css">
     <title>Foodstagram</title>
 </head>
 <body>
-
+    
+    <nav class="navbar">
+        <a class="navbar__btn" href="#">home</a>
+        <a class="nabbar__btn" href="#">new</a>
+        <a class="navbar__btn" href="#">settings</a>
+    
+    </nav>
     
 </body>
 </html>
--- login.php
+++ login.php
@@ -11,9 +11,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="style/style.css">
 </head>
 <body>
-   
+   <h1>yey</h1>
     
 </body>
 </html>
