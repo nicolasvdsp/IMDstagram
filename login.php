92df443a6b019f035a98b181bbe60ec11a1725b5
nicolasvdsp <nicolasvds@live.be>
2021-04-27 00:27:06 +0200

    adds login

--- login.php
+++ login.php
@@ -11,19 +11,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" type="text/css" href="style/style.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-    <img src="assets/logo_dinkstagram.svg" alt="Logo Dinkstagram">
     <div class="login">
+        <img class="logo--login" src="assets/logo_dinkstagram.svg" alt="Logo Dinkstagram">
         <form action="" method="GET">
             <div class="form__input">
                 <label for="email">Email</label>
-                <input type="text" id="email" name="email">
+                <input type="text" id="email" name="email" placeholder="vjtony@w&m.be">
             </div> 
             <div class="form__input">
                 <label for="password">Password</label>
-                <input type="text" id="password" name="password">
+                <input type="text" id="password" name="password" placeholder="• • • • • • • • • •">
             </div> 
         </form>
     </div>
