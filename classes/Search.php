bfb244fa4db32182b3432cb664991547a22776fb
Maryam <r0739428@student.thomasmore.be>
2021-05-25 16:02:50 +0200

    Feat: search PDO fixed

--- classes/Search.php
+++ classes/Search.php
@@ -1,48 +1,47 @@
 <?php
 include_once(__DIR__ . "/Db.php");
 
+//$search = $_GET['q'];
+
 class Search{
- 
-
-
-    public  function search($searchUser){
+    //GEBRUIKER OPZOEKEN
+  public function searchUser($searchUser){
         $conn = Db::getConnection();
-        $search_result = $conn->query('SELECT * FROM( select id as uid, firstname, lastname, profile_picture, null as tag, null as 
-        upload_location from users union select null as uid, null as firstname, null as lastname, null as profile_picture, tag, upload_location from post ) AS t WHERE firstname LIKE "%'.$q.'%" OR lastname LIKE "%'.$q.'%"');
-        
-        $search_result->bindValue(":search", '%'.$searchUser.'%');
+        $search_result = $conn->prepare('SELECT * FROM( select id as uid, firstname, lastname, profile_picture, null as tag, null as 
+        upload_location from users union select null as uid, null as firstname, null as lastname, null as profile_picture, tag, upload_location from post ) AS t WHERE firstname LIKE :search OR lastname LIKE :search ');
+        $searchItem = "%".$searchUser."%";
+        $search_result->bindParam(":search", $searchItem);
         $search_result->execute();
-        return $search_result;
-
-        $results = $search_result->fetchAll();
-
+        $value = $search_result->fetchAll(); 
+        return $value; 
     }
 
-
-    
-
-   /* public function searchTags($searchTags){
+    //TAG OPZOEKEN
+    public function searchTag($searchTag){
         $conn = Db::getConnection();
-        $search_result = $conn->query('SELECT * FROM( select id as uid, firstname, lastname, profile_picture, null as tag, null as 
-        upload_location from users union select null as uid, null as firstname, null as lastname, null as profile_picture, tag, upload_location from post ) AS t WHERE tag LIKE "%'.$q.'%"');
-        
-        $search_result->bindValue(":search", '%'.$searchTags.'%');
-        $search_result->execute();  
-    }
-*/
-    /*public function searchLocation($searchLocation){
-        $conn = Db::getConnection();
-        $search_result = $conn->query('SELECT * FROM( select id as uid, firstname, lastname, profile_picture, null as tag, null as 
-        upload_location from users union select null as uid, null as firstname, null as lastname, null as profile_picture, tag, upload_location from post ) AS t WHERE  upload_location LIKE "%'.$q.'%"');
-       
-       $search_result->bindValue(":search", '%'.$searchLocation.'%');
+        $search_result = $conn->prepare('SELECT * FROM( select id as uid, firstname, lastname, profile_picture, null as tag, null as 
+        upload_location from users union select null as uid, null as firstname, null as lastname, null as profile_picture, tag, upload_location from post ) AS t WHERE tag LIKE :search');
+        $searchItem = "%".$searchTag."%";
+        $search_result->bindParam(":search", $searchItem);
         $search_result->execute();
+        $value = $search_result->fetchAll(); 
+        return $value; 
     }
-*/
+
+
+    //LOCATIE OPZOEKEN
+    public function searchLocation($searchLocation){
+        $conn = Db::getConnection();
+        $search_result = $conn->prepare('SELECT * FROM( select id as uid, firstname, lastname, profile_picture, null as tag, null as 
+        upload_location from users union select null as uid, null as firstname, null as lastname, null as profile_picture, tag, upload_location from post ) AS t WHERE location LIKE :search');
+        $searchItem = "%".$searchLocation."%";
+        $search_result->bindParam(":search", $searchItem);
+        $search_result->execute();
+        $value = $search_result->fetchAll(); 
+        return $value; 
+    }
 
 }
-
-
 
 
 ?>
---
+++ test.php
@@ -0,0 +1,96 @@
+<?php
+include_once(__DIR__ . "/classes/Db.php");
+include_once(__DIR__. "/classes/User.php");
+include_once(__DIR__ . "/classes/Post.php");
+include_once(__DIR__ . "/classes/Search.php");
+
+
+ini_set('display_errors', true);
+
+//database connectie
+$conn = Db::getConnection();
+
+//Sessie starten
+session_start();
+if(!isset($_SESSION['id'])) {
+    header('location: login.php');
+} else{
+    $sessionId = $_SESSION['id'];
+}
+
+$search = new Search();
+
+if(!empty($_GET)) {
+  $user = $search->searchUser($_GET['q']);
+  $tag = $search->searchTag($_GET['q']);
+  $location = $search->searchLocation($_GET['q']);
+
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="css/reset.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<body>
+
+<div class="header">
+    <a href="index.php"><img class="logo" src="./assets/logo_dinkstagram.svg" alt="Logo Dinkstagram"/></a>
+</div>
+
+<section class="posts">
+    <!--Zoekformulier-->
+    <form action="" method="GET">
+        <input type="search" name="q" placeholder="Search ..." />
+        <input type="submit" value="Enter" />
+    </form>
+</section>
+
+<!--Search tabs-->
+<div class="tab">
+  <button class="tablinks" onclick="openCity(event, 'Accounts')">Accounts</button>
+  <button class="tablinks" onclick="openCity(event, 'Tags')">#Tags</button>
+  <button class="tablinks" onclick="openCity(event, 'Locations')">Locations</button>
+</div>
+
+<!--Accounts-->
+<div id="Accounts" class="tabcontent">
+  <ul>
+  <?php if(isset($user)): ?>
+  <?php foreach($user as $account) { ?>
+        <li style="font-size: 20px;"><a href="index.php" ><img style="width: 50px; height:50px; border-radius: 100%;" src="<?php echo $account['profile_picture']; ?>" alt="avatar">
+          <?=$account['firstname'].' '.$account['lastname']?></a><br><br></li>
+      <?php } ?>
+    <?php endif; ?>
+  </ul> 
+
+</div>
+
+<!--Tags-->
+<div id="Tags" class="tabcontent">
+  <ul>
+  <?php if(isset($tag)): ?>
+  <?php foreach($tag as $account) { ?>
+      
+        
+    <?php } ?>
+  <?php endif; ?>
+  </ul> 
+</div>
+
+
+<!--Location-->
+<div id="Locations" class="tabcontent">
+  <ul>
+
+  </ul> 
+
+</div>
+
+<script src="script.js"></script>
+
+   
+</body>
+</html> 
